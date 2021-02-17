7dfafb74c676a2e0b0f8eed0511df5e2acbcd6c5
Ollie James <osj.james@gmail.com>
2021-02-17 23:15:13 +0000

    Update class binding fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+// Used https://dev.to/swyx/how-to-set-up-svelte-with-tailwind-css-4fg5
+
 const isProduction = !process.env.ROLLUP_WATCH; // or some other env var like NODE_ENV
 module.exports = {
     plugins: [
@@ -9,14 +11,11 @@ module.exports = {
             // may also want to include HTML files
             // "./src/**/*.html"
         ],
-        // this is for extracting Svelte `class:` syntax but is not perfect yet, see below
-        defaultExtractor: content => {
-            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-            const broadMatchesWithoutTrailingSlash = broadMatches.map(match => _.trimEnd(match, '\\'))
-            const matches = broadMatches
-                .concat(broadMatchesWithoutTrailingSlash)
-            return matches
-        },
+        // this is for extracting Svelte `class:` syntax. Got from https://github.com/tailwindlabs/tailwindcss/discussions/1731#discussioncomment-294774
+        defaultExtractor: content => [
+            ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+            ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+        ],
         enabled: isProduction // disable purge in dev
     },
 };
